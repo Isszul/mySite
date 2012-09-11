5ce66e595036d9e6d0325d38175c29fa9999e9ed
Isszul <Isszul@gmail.com>
2012-09-11 19:59:50 +0100

    Removed unneeded import of process

--- routes/reload.js
+++ routes/reload.js
@@ -2,9 +2,8 @@
  * Kills the app (allows the bash script to cicle and reload)
  */
 
-var process = require('process');
 
-exports.runUpdate = function(req, res){
+exports.reload = function(req, res){
 
   res.render('reload', { 
   	title: 'Reloading from GitHub'
