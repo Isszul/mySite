c165dedc54b9bffb79820062102f70bce92173fb
Isszul <Isszul@gmail.com>
2012-09-12 02:06:58 +0100

    Added another test blog

--- routes/blog.js
+++ routes/blog.js
@@ -3,7 +3,18 @@
  */
 var linking = require('./linking');
 
-var blogEntries = [{title: 'Test title', text:'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum', submitted: '11/09/2012'}];
+var blogEntries = [
+	{
+		title: 'Test title', 
+		text:'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum', 
+		submitted: '11/09/2012'
+	},
+	{
+		title: 'Test title 2', 
+		text:'Lorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem IpsumLorem Ipsum', 
+		submitted: '12/09/2012'
+	},	
+];
 
 
 exports.blog = function(req, res){
