93604c1f5eac987b64716fa33485ea672c94cf6b
isszul <Isszul@gmail.com>
2012-09-15 17:55:01 +0100

    Reverted last

--- app.js
+++ app.js
@@ -9,41 +9,41 @@ var express = require('express'),
   path = require('path'),
   nowServerSide = require("./nowServerSide/nowServerSide.js");
 
-var express = express();
+var app = express();
 
 var store  = new express.session.MemoryStore;
 
 
-express.configure(function(){
-  express.set('port', process.env.PORT || 3000);
-  express.set('views', __dirname + '/views');
-  express.set('view engine', 'jade');
-  express.use(express.favicon());
-  express.use(express.logger('dev'));
-  express.use(express.bodyParser());
-  express.use(express.methodOverride());
-  express.use(express.cookieParser());
-  express.use(express.session({ secret: 'Mdc8D8o3zzZDe__=1+2', store: store }));
-  express.use(express.router);
-  express.use(require('stylus').middleware(__dirname + '/public'));
-  express.use(express.static(path.join(__dirname, 'public')));
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser());
+  app.use(express.session({ secret: 'Mdc8D8o3zzZDe__=1+2', store: store }));
+  app.use(express.router);
+  app.use(require('stylus').middleware(__dirname + '/public'));
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-express.configure('development', function(){
-  express.use(express.errorHandler());
+app.configure('development', function(){
+  app.use(express.errorHandler());
 });
 
 
 
 var links = linking.getAllLinks();
 for (var i = 0; i < links.length; i++) {
-  express.all(links[i].href, links[i].routingFunc);
+  app.all(links[i].href, links[i].routingFunc);
 };
 
-var expressServer = http.createServer(express);
+var expressServer = http.createServer(app);
 
-expressServer.listen(express.get('port'), function(){
-  console.log("Express server listening on port " + express.get('port'));
+expressServer.listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
 });
 
 
